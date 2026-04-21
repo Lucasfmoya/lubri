ede8a145813570208b6790d71cdaa92112db7003
Lucasfmoya <lucasmoyafe@gmail.com>
2026-04-20 22:00:24 -0300

    modificando navbar dev

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".navbar");
+  const nav = document.querySelector(".navbar");
   if (window.scrollY > 50) {
     navbar.classList.add("navbar-scrolled");
   } else {
     navbar.classList.remove("navbar-scrolled");
   }
+});
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
 });
