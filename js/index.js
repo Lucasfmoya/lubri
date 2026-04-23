ab84925411382a1108b66530d77d4b26694c3e3b
Lucas Moya <lucasmoyafe@gmail.com>
2026-04-23 16:04:10 -0300

    navbar con efecto azul y blanco

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-window.addEventListener("scroll", function () {
-  const nav = document.querySelector(".navbar");
-  if (window.scrollY > 50) {
+
+const navbar = document.querySelector(".navbar");
+
+let isScrolled = false;
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  // Activa cuando baja lo suficiente
+  if (!isScrolled && scrollY > 80) {
     navbar.classList.add("navbar-scrolled");
-  } else {
-    navbar.classList.remove("navbar-scrolled");
+    isScrolled = true;
   }
-});
 
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-
-abrir.addEventListener("click", () => {
-  nav.classList.add("visible");
-});
-
-cerrar.addEventListener("click", () => {
-  nav.classList.remove("visible");
+  // Desactiva cuando sube bastante
+  if (isScrolled && scrollY < 40) {
+    navbar.classList.remove("navbar-scrolled");
+    isScrolled = false;
+  }
 });
