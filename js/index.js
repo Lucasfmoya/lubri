bc3b0e3b718c0a9b86411d3e82913d90417c67be
Lucas Moya <lucasmoyafe@gmail.com>
2026-04-27 18:20:37 -0300

    agregando animaciones con js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/*Suavizar el efecto del navbar al hacer scroll*/
+
 const navbar = document.querySelector(".navbar");
 
 let isScrolled = false;
@@ -23,3 +25,29 @@ if (toggler) {
     toggler.classList.toggle("open");
   });
 }
+
+/* función para agregar animaciones */
+
+const elements = document.querySelectorAll("[data-animate]");
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
+
+      const animation = entry.target.dataset.animate;
+
+      entry.target.classList.add(animation);
+      entry.target.classList.add("show");
+
+      // 🔒 clave: deja de observarlo para siempre
+      observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.2,
+    rootMargin: "0px 0px -80px 0px",
+  }
+);
+
+elements.forEach((el) => observer.observe(el));
